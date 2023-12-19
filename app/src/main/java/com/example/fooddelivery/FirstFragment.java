636a99454bf12a276fafcca9212bb431a21faac7
Syllc <124968985+Syllc@users.noreply.github.com>
2023-12-19 19:05:25 +0700

    main java home page

--- app/src/main/java/com/example/fooddelivery/FirstFragment.java
+++ app/src/main/java/com/example/fooddelivery/FirstFragment.java
@@ -6,16 +6,74 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.fooddelivery.FoodRecommendationFragment;
+import com.example.fooddelivery.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class FirstFragment extends Fragment {
-    View myView;
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
 
-        return myView;
+        viewPager = view.findViewById(R.id.viewPager);
+        tabLayout = view.findViewById(R.id.tabLayout);
+
+        setupViewPager();
+
+        return view;
+    }
+
+    private void setupViewPager() {
+        FoodPagerAdapter adapter = new FoodPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new FoodRecommendationFragment(), "Recommendation 1");
+        adapter.addFragment(new FoodRecommendationFragment(), "Recommendation 2");
+        adapter.addFragment(new FoodRecommendationFragment(), "Recommendation 3");
+        // Add more fragments as needed
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private static class FoodPagerAdapter extends FragmentPagerAdapter {
+
+        private final java.util.List<Fragment> fragmentList = new java.util.ArrayList<>();
+        private final java.util.List<String> fragmentTitleList = new java.util.ArrayList<>();
+
+        public FoodPagerAdapter(FragmentManager manager) {
+            super(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }
--- app/src/main/java/com/example/fooddelivery/FoodRecommendationFragment.java
+++ app/src/main/java/com/example/fooddelivery/FoodRecommendationFragment.java
@@ -1,18 +1,22 @@
 package com.example.fooddelivery;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class ThirdFragment extends Fragment {
+
+import androidx.fragment.app.Fragment;
+
+public class FoodRecommendationFragment extends Fragment {
+
+    public FoodRecommendationFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        return inflater.inflate(R.layout.item_food_recommendation, container, false);
     }
 }
